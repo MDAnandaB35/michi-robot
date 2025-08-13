71c85a647138461766b391b5859518e811e81de3
mdanandab35 <daffananda2020@gmail.com>
2025-08-13 19:58:23 +0700

    modified allowed cors origin to all

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite dev server default port
+  origin: "*", // Vite dev server default port
   credentials: true
 }));
 
